34d3809b85da7adb6ecd4fffaf3d09d08550f09c
Pg <pg.developper.fr@gmail.com>
2021-12-25 18:48:02 +0100

    Implement retry

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,36 @@
 
 
 SVN_URL="$1"
-
-ls -la /github/workspace
+AC_VERBOSE="$2"
+RETRY="$3"
 
 # Check if action already init
 (test -f .svn2git/svn-config && SVN_INIT=true) || SVN_INIT=false
 
 if [ "$SVN_INIT" = false ]
 then
-  GIT_TRACE=2 GIT_CURL_VERBOSE=2 GIT_TRACE_PERFORMANCE=2 GIT_TRACE_PACK_ACCESS=2 GIT_TRACE_PACKET=2 GIT_TRACE_PACKFILE=2 GIT_TRACE_SETUP=2 GIT_TRACE_SHALLOW=2 svn2git "$SVN_URL"
+  set +e
+  svn2git "$SVN_URL" > /dev/null
+  if ! $?
+  then
+    echo "svn2git failed..."
+    if [[ $RETRY -ge 0 ]]; then
+      try=0
+      while [[ $try -lt $RETRY ]]; do
+        try=$(($try+1))
+        echo "Retry $try"
+        svn2git --rebase
+        result=$?
+        if [[ $result ]]; then
+          #success
+          break
+        fi
+        echo "svn2git --rebase failed..."
+      done
+    fi
+  fi
 
+  set -e
   # Saving the config
   mkdir -p .svn2git
   git config --get-regexp svn-remote.svn > .svn2git/svn-config
