fa18f3684db41f974d4d6b1912a3945d8a907295
Pg <pg.developper.fr@gmail.com>
2022-03-15 22:57:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,20 @@ SVN_URL="$1"
 AC_VERBOSE="$2"
 RETRY="$3"
 
+watch_res() {
+  printf "Memory\t\tDisk\t\tCPU\n"
+  end=$((SECONDS+3600))
+  while [ $SECONDS -lt $end ]; do
+    MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+    DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+    CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+    echo "$MEMORY$DISK$CPU"
+    sleep 30
+  done
+}
+
+watch_res &
+
 git config --global gc.auto 0
 git config --global user.name "github-actions[bot]"
 git config --global user.email "4815162342+github-actions[bot]@users.noreply.github.com"
