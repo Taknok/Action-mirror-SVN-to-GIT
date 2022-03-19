f3fb252a30b5669a6faa0aa7482fbf3363e74ce9
Pg <pg.developper.fr@gmail.com>
2022-03-20 00:16:37 +0100

    Simplification

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,6 @@ WORKDIR /github/workspace
 
 COPY entrypoint.sh /entrypoint.sh
 
-RUN chmod u+x /entrypoint.sh && apt-get update && apt-get install -y git-svn ruby procps
-
-RUN git clone https://github.com/Taknok/svn2git.git && cd svn2git && git checkout dev
-
-RUN cd svn2git && gem build svn2git.gemspec -o svn2git.gem && gem install svn2git.gem
+RUN chmod u+x /entrypoint.sh && apt-get update && apt-get install -y git-svn
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -23,54 +23,10 @@ git config --global gc.auto 0
 git config --global user.name "github-actions[bot]"
 git config --global user.email "4815162342+github-actions[bot]@users.noreply.github.com"
 
-# Check if action already init
-(test -f .svn2git/svn-config && SVN_INIT=true) || SVN_INIT=false
-
-# 2 svn2git have to be run, because first convert svn2git config from v1 to v2 
-# but do not rebase into master. Once migration is done, rebase into master
-# is done
-
-save_svn_config () {
-  mkdir -p .svn2git
-  git config --get-regexp svn-remote.svn > .svn2git/svn-config
-  cp -r .git/svn .svn2git/
-  git add .svn2git/
-  git commit -m "Save svn config to .svn2git/svn-config"
-}
-
-silent () {
-  if [[ "$AC_VERBOSE" == "true" ]]; then
-    $@
-  else
-    $@ > /dev/null
-  fi
-}
-
-clone() {
-  git svn init "$SVN_URL" --no-metadata --stdlayout --prefix='svn/'
-  git svn fetch
-}
-
-if [ "$SVN_INIT" = false ]
-then
-  # first run
-  echo "First run, initializing project"
-  set +e
-  # fetch and create everything except for master
-  silent clone
-
-  # Saving the config
-  save_svn_config
-else
-  # already initialized
-  echo "Already initialized, loading configuration"
-  # Loading config
-  cat .svn2git/svn-config | while read line; do git config $line; done
-  cp -r .svn2git/svn .git/
-  # fetch and create everything except for master
-fi
+git svn init "$SVN_URL" --no-metadata --stdlayout --prefix='svn/'
+svn2git --rebase
 
 # Optimizing repo
 git gc --auto
 
-#git push --mirror
+git push
